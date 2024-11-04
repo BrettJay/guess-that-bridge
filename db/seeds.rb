33ac98f303a695f87d991a9eeb77a2e43c070525
Brett Jones <677025+BrettJay@users.noreply.github.com>
2024-11-04 10:27:42 +0800

    Update seeds with example models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,12 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(email_address: 'email@example.com', password: 'abc123', password_confirmation: 'abc123')
+# Create a sample user
+sample_user = User.create!(email_address: 'email@example.com', password: 'abc123', password_confirmation: 'abc123')
+
+# Create a same game with a question
+sample_game = Game.create!(title: '8 November 2024')
+sample_question = Question.create!(title: 'What is this bridge?', answer: 'The empire state building', game: sample_game)
+
+# Create a GameUser association
+sample_game_user = GameUser.create!(game: sample_game, user: sample_user)
