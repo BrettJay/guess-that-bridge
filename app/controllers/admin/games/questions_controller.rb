151517ca49a20fbb25fc803a1c3c670e9844618f
Brett Jones <677025+BrettJay@users.noreply.github.com>
2024-11-11 20:33:19 +0800

    Enable the ability to add a question for a game

--- app/controllers/admin/games/questions_controller.rb
+++ app/controllers/admin/games/questions_controller.rb
@@ -1,7 +1,19 @@
 class Admin::Games::QuestionsController < Admin::BaseController
+  before_action :set_game, only: %i[ new create]
+
   def new
+    @question = Question.new
   end
 
   def create
   end
+
+  private
+    def set_game
+      @game = Game.find(params.expect(:game_id))
+    end
+
+    def question_params
+      params.expect(question: [ :title, :answer ])
+    end
 end
