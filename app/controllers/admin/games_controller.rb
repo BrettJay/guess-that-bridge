ce08ebabba9fc8138685779abbe4beff4eaeb881
Brett Jones <677025+BrettJay@users.noreply.github.com>
2024-11-11 10:46:26 +0800

    Attempt a fix

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -65,6 +65,6 @@ class Admin::GamesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def game_params
-      params.fetch(:game, {})
+      params.fetch(:game, [:title])
     end
 end
